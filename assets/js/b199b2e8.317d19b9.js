0ebf79367e267878f645302382cda5b364673187
Nick Vidal <nick@profian.com>
2021-11-03 22:26:31 -0300

    Adding Resources section

---
+++ assets/js/b199b2e8.317d19b9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[5419],{3568:function(s){s.exports=JSON.parse('{"allTagsPath":"/resources/tags","slug":"/resources/tags/podcast","name":"Podcast","count":1,"permalink":"/resources/tags/podcast"}')}}]);
