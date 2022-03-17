50f678e345f62e54b5e39fbf1a3111f742c65c1a
Nick Vidal <nick@profian.com>
2022-03-17 16:58:01 -0300

    feat: add mermaid support
    Signed-off-by: Nick Vidal <nick@profian.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/enarx/enarx.github.io/blob/main',
+          remarkPlugins: [require('mdx-mermaid')],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
